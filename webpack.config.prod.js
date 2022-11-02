c09d0ab8f1c2c409aa8a9e4b847a1729d7d68014
suguru-3u <suguru.13579515@gmail.com>
2022-11-02 09:45:49 +0900

    study webpack

---
+++ webpack.config.prod.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const cleanPlugin = require("clean-webpack-plugin");
+const { CleanPlugin } = require("webpack");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/app.ts",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devtool: false,
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  plugins: [new CleanPlugin.CleanWebpackPlugin()],
+};
